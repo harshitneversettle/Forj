880f8d0493d08c25a0c9f4473c8d7aa547195cb1
Harshit yadav <harshityadav5499@gmail.com>
2026-01-12 00:00:41 +0530

    Add : claim cert route

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -146,3 +146,62 @@ app.post(
     res.send(data);
   }
 );
+
+
+app.post("/api/claim", async (req, res) => {
+  const issuer = req.body.pubkey;
+  const issuerPubkey = new PublicKey(issuer!);
+  const uniqueKey = req.body.uniqueKey;
+  const studentEmail = req.body.userEmail;
+  const [eventPda] = PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("event"),
+      issuerPubkey!.toBuffer(),
+      new BN(Number(uniqueKey)).toArrayLike(Buffer, "le", 8),
+    ],
+    programId
+  );
+  const eventState = await (program.account as any).event.fetch(eventPda);
+  const metadataUrl = eventState.metadataUri;
+  const templateUri = eventState.templateUri;
+  const result = await fetch(metadataUrl);
+  const eventName = eventState.eventName;
+  const data = await result.json();
+  let ans_name;
+  let ans_enroll;
+  let ans_email;
+  let ans_position;
+  let ans_eventName = eventName;
+  let verify_url = `http://localhost:5173/verify/${issuer}/${uniqueKey}`;
+  (data as []).map((i: any) => {
+    let currEmail = i.email;
+    if (currEmail === studentEmail) {
+      ans_name = i.name;
+      ans_enroll = i.enroll || null;
+      ans_email = i.email || null;
+      ans_position = i.position || null;
+    }
+  });
+
+  const response = {
+    ans_name,
+    ans_enroll,
+    ans_email,
+    ans_position,
+    ans_eventName,
+    templateUri,
+    eventName,
+    verify_url,
+  };
+  console.log(`http://localhost:5173/verify/${issuer}/${uniqueKey}`);
+  if (
+    ans_name === undefined ||
+    ans_enroll === undefined ||
+    ans_name === "" ||
+    ans_enroll === ""
+  ) {
+    res.status(400).send("error");
+  } else {
+    res.json(response);
+  }
+});
