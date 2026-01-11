8128aa56a921923699baef6fe1c1bf38debc7fbe
Harshit yadav <harshityadav5499@gmail.com>
2026-01-12 00:02:22 +0530

    not able to commit all the code during building , bad practice

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -241,3 +241,82 @@ app.post("/api/generate-certificate", async (req, res) => {
   res.send(Buffer.from(pdfBytes));
 });
 
+
+app.post("/api/verify", async (req, res) => {
+  try {
+    const { issuer, uniqueKey, email } = req.body;
+    const issuerPubkey = new PublicKey(issuer!);
+    //console.log(issuerPubkey.toBase58());
+    const [eventPda, bump] = await PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("event"),
+        issuerPubkey.toBuffer(),
+        new BN(Number(uniqueKey)).toArrayLike(Buffer, "le", 8),
+      ],
+      programId
+    );
+    //console.log(eventPda.toString());
+    const eventState = await (program.account as any).event.fetch(eventPda);
+    const dataUrl = eventState.metadataUri;
+    const merkleProofUrl = eventState.merkleProofUri;
+    const merkleRoot = Buffer.from(
+      eventState.merkleRoot.buffer || eventState.merkleRoot
+    );
+    //console.log(merkleProofUri);
+    const result1 = await fetch(merkleProofUrl);
+    const merkleProof = await result1.json();
+    const result2 = await fetch(dataUrl);
+    const studentData = await result2.json();
+    //console.log(studentData);
+    let result_name;
+    let result_enroll;
+    let result_email;
+    let result_position;
+    let student: any = null;
+    let corress_idx = -1;
+    (studentData as []).forEach((i: any, index: number) => {
+      if (i.email === email) {
+        student = {
+          email: i.email,
+          enroll: i.enroll,
+          name: i.name,
+          position: i.position,
+        };
+        corress_idx = index;
+      }
+    });
+    //console.log(corress_idx);
+    const stringify = JSON.stringify(student, Object.keys(student));
+    let leafHash = crypto.createHash("sha256").update(stringify).digest();
+    const proof = (merkleProof as [])[corress_idx] as any[];
+    for (const i of proof) {
+      const cleanHex = i.toString().startsWith("0x") ? i.slice(2) : i;
+      const proofBuffer = Buffer.from(cleanHex, "hex");
+      if (leafHash.compare(proofBuffer) < 0) {
+        leafHash = crypto
+          .createHash("sha256")
+          .update(Buffer.concat([leafHash, proofBuffer]))
+          .digest();
+      } else {
+        leafHash = crypto
+          .createHash("sha256")
+          .update(Buffer.concat([proofBuffer, leafHash]))
+          .digest();
+      }
+    }
+    console.log(
+      "Expected root:",
+      Buffer.from(eventState.merkleRoot).toString("hex")
+    );
+    const verified = leafHash.equals(merkleRoot);
+    console.log(verified);
+    res.send(verified);
+  } catch (error) {
+    console.log("lol");
+    res.send("lol");
+  }
+});
+
+app.listen(3001, () => {
+  console.log(`Server running on http://localhost:${3001}`);
+});
