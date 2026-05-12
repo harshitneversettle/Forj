1917c5c48837d05e1f2fe369c17c5c9469a8e87a
Harshit yadav <harshityadav5499@gmail.com>
2026-05-12 23:49:09 +0530

    changed link

--- Frontend/src/components/Demo.tsx
+++ Frontend/src/components/Demo.tsx
@@ -1,13 +1,18 @@
 export default function Demo() {
   return (
-    <>
-      <div className="relative pb-[62.5%] h-0">
-        <iframe
-          src="https://www.loom.com/share/75243a58ff83437e917b3c3a0a3feddd"
-          allowFullScreen
-          className="absolute top-0 left-0 w-full h-full"
-        />
-      </div>
-    </>
+    <div style={{ position: "relative", paddingBottom: "62.5%", height: 0 }}>
+      <iframe
+        src="https://www.loom.com/embed/75243a58ff83437e917b3c3a0a3feddd"
+        frameBorder="0"
+        allowFullScreen
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      />
+    </div>
   );
 }
