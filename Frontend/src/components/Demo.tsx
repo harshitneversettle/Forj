25b3ea89d739fc2a0e4faf1b2d37a7b1b98db7ee
Harshit yadav <harshityadav5499@gmail.com>
2026-05-12 23:44:46 +0530

    changed link

--- Frontend/src/components/Demo.tsx
+++ Frontend/src/components/Demo.tsx
@@ -1,20 +1,13 @@
-const Demo = ({
-  src = "https://www.loom.com/embed/17495acb8afc47ae8ca3636aaad23e96",
-}) => (
-  <div style={{ position: "relative", paddingBottom: "62.5%", height: 0 }}>
-    <iframe
-      src={src}
-      frameBorder="0"
-      allowFullScreen
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-      }}
-    />
-  </div>
-);
-
-export default Demo;
+export default function Demo() {
+  return (
+    <>
+      <div className="relative pb-[62.5%] h-0">
+        <iframe
+          src="https://www.loom.com/share/75243a58ff83437e917b3c3a0a3feddd"
+          allowFullScreen
+          className="absolute top-0 left-0 w-full h-full"
+        />
+      </div>
+    </>
+  );
+}
